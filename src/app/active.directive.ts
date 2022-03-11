d3b0a98dfb69e7dfe14e9e7aa8e80b41873b4c2d
LAPTOP-IP5FO39M\KhushiAgrawal <yashagrawal1267@gmail.com>
2022-03-11 23:28:50 +0530

    Test Exercise

---
+++ src/app/active.directive.ts
@@ -0,0 +1,10 @@
+import { Directive,ElementRef } from '@angular/core';
+
+@Directive({
+  selector: '[appActive]'
+})
+export class ActiveDirective {
+ constructor(el:ElementRef) {
+    el.nativeElement.style.backgroundColor='Green';
+   }
+}
